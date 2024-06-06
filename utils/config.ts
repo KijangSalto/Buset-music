7a0125ef3c12a55545c9a13a314fba6ef1f65eae
Kijang Salto <77543963+KijangSalto@users.noreply.github.com>
2024-06-07 02:55:09 +0700

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -7,10 +7,10 @@ try {
   config = require("../config.json");
 } catch (error) {
   config = {
-    TOKEN: process.env.TOKEN || "",
-    MAX_PLAYLIST_SIZE: parseInt(process.env.MAX_PLAYLIST_SIZE!) || 10,
+    TOKEN: process.env['TOKEN'] || "",
+    MAX_PLAYLIST_SIZE: parseInt(process.env.MAX_PLAYLIST_SIZE!) || 100,
     PRUNING: process.env.PRUNING === "true" ? true : false,
-    STAY_TIME: parseInt(process.env.STAY_TIME!) || 30,
+    STAY_TIME: parseInt(process.env.STAY_TIME!) || 300,
     DEFAULT_VOLUME: parseInt(process.env.DEFAULT_VOLUME!) || 100,
     LOCALE: process.env.LOCALE || "en"
   };
